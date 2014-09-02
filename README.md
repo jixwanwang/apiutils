6d2eceae86abae8897f247fdaee5f16bef734e42
jwang <jwang@justin.tv>
2014-09-02 14:04:33 -0700

    readme

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
 apiutils
 ========
 
-Helpful API serving utilities
+Helpful API handling utilities.
+
+
+## Getting Started
+
+  go get github.com/jixwanwang/apiutils
+
+## Example Usage:
+
+```
+package main
+
+import (
+        "log"
+        "net/http"
+
+        "github.com/jixwanwang/apiutils"
+)
+
+func main() {
+        http.HandleFunc("/dowork", workwork)
+
+        if err := http.ListenAndServe(":8080", http.DefaultServeMux); err != nil {
+                log.Fatalf("Could not start server: %s", err.Error())
+        }
+}
+
+func workwork(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		err := apiutils.RequireParams(query, []string{"foo", "bar"})
+		if err != nil {
+			apiutils.ServeError(w, apiutils.NewErrorResponse(http.StatusBadRequest, err.Error()))
+			return
+		}
+        apiutils.ServeJSON(w, []string{"Hello!", "World"})
+}
+```
--- apiutils.go
+++ apiutils.go
@@ -8,17 +8,6 @@ import (
 	"strconv"
 )
 
-func ServeJSON(w http.ResponseWriter, v interface{}) {
-	content, err := json.MarshalIndent(v, "", "  ")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Length", strconv.Itoa(len(content)))
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(content)
-}
-
 func RequireParams(form url.Values, params []string) error {
 	for _, param := range params {
 		if len(form[param]) == 0 {
@@ -40,6 +29,17 @@ func NewErrorResponse(status int, message string) ErrorResponse {
 		Message: message,
 		Error:   http.StatusText(status),
 	}
+}
+
+func ServeJSON(w http.ResponseWriter, v interface{}) {
+	content, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Length", strconv.Itoa(len(content)))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(content)
 }
 
 func ServeError(w http.ResponseWriter, errRes ErrorResponse) {
